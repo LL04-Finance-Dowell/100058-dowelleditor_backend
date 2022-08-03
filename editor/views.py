fd07c20f1d32eae06828f2387d135e12e6af7ed0
hassan1731996 <hassan1731996@gmail.com>
2022-08-03 19:42:30 +0500

    changed JsonResponse to Response

--- editor/views.py
+++ editor/views.py
@@ -1,8 +1,6 @@
-import json
-
-from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -17,10 +15,9 @@ class GetAllDataByCollection(APIView):
         collection = request.data.get('collection', None)
         fields = request.data.get('fields', None)
         if database and collection and fields:
-            return HttpResponse(
-                json.dumps(targeted_population(database, collection, [fields], 'life_time'),
-                           indent=2, sort_keys=True))
-        return JsonResponse({"info": "all parameters are required, database, collection, fields"})
+            return Response(targeted_population(database, collection, [fields], 'life_time'), status=status.HTTP_200_OK)
+        return Response({"info": "all parameters are required, database, collection, fields"},
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -34,6 +31,6 @@ class PostDataIntoCollection(APIView):
             res = save_data_into_collection(database, collection, fields)
             population_res = targeted_population(database, collection, ['_id'], 'life_time')
             if filter_res := filter_id('_id', res['inserted_id'], population_res['normal']['data'][0]):
-                return Response(
-                    json.dumps(filter_res, indent=2, sort_keys=True))
-        return JsonResponse({"info": "all parameters are required, database, collection, fields"})
+                return Response(filter_res, status=status.HTTP_200_OK)
+        return Response({"info": "all parameters are required, database, collection, fields"},
+                        status=status.HTTP_400_BAD_REQUEST)
