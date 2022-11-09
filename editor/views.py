d20bc0bbdf53d7785fdc46d7bf36fa995e7ccf68
Manishdowellresearch <manish@dowellresearch.in>
2022-11-09 13:06:43 +0530

    ok

--- editor/views.py
+++ editor/views.py
@@ -1,10 +1,11 @@
+import json
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from editor.utils import targeted_population, filter_id
+from editor.utils import targeted_population, filter_id , get_event_id, dowellconnection
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -32,14 +33,16 @@ class GetAllDataByCollection(APIView):
 class PostDataIntoCollection(APIView):
 
     def post(self, request):
-        database = request.data.get('database', None)
-        collection = request.data.get('collection', None)
-        fields = request.data.get('fields', None)
-        if database and collection and fields:
-            field_name = fields['field_name']
-            population_res = targeted_population(database, collection, [field_name], 'life_time')
-            if filter_res := filter_id('_id', fields['inserted_id']['inserted_id'],
-                                       population_res['normal']['data'][0]):
-                return Response(filter_res, status=status.HTTP_200_OK)
-        return Response({"info": "all parameters are required, database, collection, fields"},
-                        status=status.HTTP_400_BAD_REQUEST)
+        if request.method == "POST":
+            raw_data = request.data.get('raw_data', None)
+            edited_data = request.data.get('edited_data', None)
+            return Response(edited_data,status=status.HTTP_200_OK)
+            """field ={
+                "eventId":get_event_id(),
+                "raw_data": raw_data,
+                "edited_data":edited_data
+            }
+            inserted_id= dowellconnection("Documents","Documentation","editor","editor","100084006","ABCDE","insert",field)"""
+            #return Response(raw_data,edited_data,status=status.HTTP_200_OK)
+        #return Response({"info": "Sorry!"},status=status.HTTP_400_BAD_REQUEST)
+         
