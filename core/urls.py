2f19fb0862875e161b351de697f367ae9404ae81
Morvin-Ian <morvinian@gmail.com>
2023-10-24 16:44:52 +0300

    pdf generated successfully

--- core/urls.py
+++ core/urls.py
@@ -15,18 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(('editor.urls', 'editor'), namespace="api")),
 ]
-
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
-    urlpatterns = urlpatterns + static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
